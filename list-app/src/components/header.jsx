05714b5d701f4b5c7d14350afa0106e5f43d8071
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2025-12-09 20:52:42 +0000

    added some styling stuff

---
+++ list-app/src/components/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import person from "../assets/Icon_person.png"
+
+
+function Header() {
+  return (
+    <div>
+        <div className='h-screen w-screen bg-[#FDFCDC] p-10'>
+          {/* Apply flex and items-center to the container holding the h1 and img */}
+          <div className='flex items-center'> 
+            <h1 className='text-4xl'>Get Organized</h1>
+            
+            {/* Remove ml-300 and the translate since flex will handle alignment. 
+                Add a class like ml-4 for some space between the text and the image. */}
+            <img 
+              src={person} 
+              alt="person" 
+              className='ml-300 h-10' // Added h-10 w-10 for a standard icon size, adjust as needed
+            />
+
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
